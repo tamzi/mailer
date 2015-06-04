51c4d30654f6d63e445d96a0f71655fa08ec5bec
Benoy Maniara <maniarab@gmail.com>
2015-06-04 19:57:29 +0300

    mailer.js uses promises

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,16 +1,25 @@
+var Promise = require('bluebird');
 var api_key = process.env.SENDGRID_API_KEY || require('../secret/credentials')['sendgrid_api_key'];
 
 module.exports = {
   sendMail: function(emailObj, applicantEmailAddr){
     var sendgrid  = require('sendgrid')(api_key);
-    sendgrid.send({
-      to:       applicantEmailAddr,
-      from:     'admissions@moringaschool.com',
-      subject:  emailObj.emailSubject,
-      html:     emailObj.emailContent
-    }, function(err, json) {
-      if (err) { return console.error(err); }
-      console.log(json);
+    return new Promise(function(resolve, reject){
+      sendgrid.send({
+        to:       applicantEmailAddr,
+        from:     'admissions@moringaschool.com',
+        subject:  emailObj.emailSubject,
+        html:     emailObj.emailContent
+      }, function(err, json) {
+        if (err) {
+          console.log("Mailing error");
+          return console.error(err);
+        }else{
+          console.log("Sent mail to: "+applicantEmailAddr);
+          console.log(json);
+          resolve(json);
+        }
+      });
     });
   }
 };
